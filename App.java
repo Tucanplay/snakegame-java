8cc01e5fbba468fb3c823466d7cd502cdc7e82d3
O-O <Kroynoreak@gmail.com>
2025-04-06 23:29:20 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,20 +1,18 @@
 import javax.swing.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int boardWidth = 600;
         int boardHeight = boardWidth;
 
-        JFrame frame = new JFrame("Snake");
-        frame.setVisible(true);
-	frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        JFrame frame = new JFrame("Snake_Game");
         SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
         frame.add(snakeGame);
         frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         snakeGame.requestFocus();
     }
 }
